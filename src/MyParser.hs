53d92f9144ba6d603c91cbffd59fae4d6d609314
ayu-mushi <ayu.mushi2015@gmail.com>
2018-04-15 15:27:07 +0900

    Abstract mostPos from lastPos and fstPos.

--- src/MyParser.hs
+++ src/MyParser.hs
@@ -6,6 +6,7 @@ import Control.Applicative ((<$>), (<*>))
 import Control.Monad.State(StateT(..),get,put,evalStateT,execStateT)
 import Control.Monad(mplus,mzero, MonadPlus)
 import Control.Monad.Trans(lift)
+import Data.Char (isLetter)
 
 
 pop :: (MonadPlus m) => StateT [a] m a
@@ -21,20 +22,18 @@ satisfy p = do
      then return x
      else mzero
 
+char :: (MonadPlus m, Eq a) => a -> StateT [a] m a
 char c = satisfy (==c)
 
-fstPos :: StateT [Char] [] a -> StateT [Char] [] a
-fstPos (StateT f) = StateT $ \s ->
-  case f s of
-       [] -> mzero
-       (p:ps) -> [p]
-
-lastPos :: StateT [Char] [] a -> StateT [Char] [] a
-lastPos (StateT f) = StateT $ \s ->
+mostPos :: (MonadPlus m) => ([(a, String)] -> (a, String)) -> StateT [Char] [] a -> StateT [Char] m a -- most of possibilities
+mostPos most (StateT f) = StateT $ \s ->
   case f s of
     [] -> mzero
-    ps -> [last ps]
+    ps -> return $ most ps
 
+fstPos, lastPos :: (MonadPlus m) => StateT [Char] [] a -> StateT [Char] m a
+fstPos = mostPos head -- first of possibilities
+lastPos = mostPos last -- last of possibilities
 
 string :: (MonadPlus m) => String -> StateT [Char] m String
 string "" = return ""
@@ -51,7 +50,10 @@ many p = many' `mplus` return [] where
     cs <- many p
     return (c++cs)
 
+singleton :: (MonadPlus m) => StateT [Char] m a -> StateT [Char] m [a]
+singleton = fmap (:[])
+
 myParser1 :: IO ()
 myParser1 = do
-  let (v::Maybe String) = evalStateT (many $ string "a" `mplus` string "b") "aaabbb22"
+  let (v::Maybe String) = evalStateT (many $ string "aaabbb"`mplus` string "a" ) "aaabbb22"
   print v
